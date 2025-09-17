5b98b633e54ae70077d7b52cbaf46b2f1779b7a6
Winorrepeat <155412365+ShawrmaM8@users.noreply.github.com>
2025-09-17 05:23:10 +0300

    Delete src directory

--- src/utils/arabicUtils.js
+++
@@ -1,9 +0,0 @@
-// Simple Gulf Arabic tokenization + normalization
-export function extractWords(text) {
-  return text
-    .replace(/[^\u0600-\u06FF\s]/g, "")
-    .split(/\s+/)
-    .filter(Boolean)
-    .map((w) => w.trim());
-}
-
